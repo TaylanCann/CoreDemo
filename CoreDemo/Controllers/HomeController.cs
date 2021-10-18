737c29b009c3fb8e9e6e9a8d9d7eafbf26664859
TaylanApm <taylan.hardal@apmsoft.net>
2021-10-18 15:43:24 +0300

    Ders 4

--- CoreDemo/Controllers/HomeController.cs
+++ CoreDemo/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace CoreDemo.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+         
     }
 }
