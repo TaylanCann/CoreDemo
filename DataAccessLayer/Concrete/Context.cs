981232f28dc70385a0e5bbde6a445f6f8e8463f8
TaylanCann <taylancanh@gmail.com>
2021-10-19 17:48:20 +0300

    last 19.10.2021

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Todo başka bilgisayara geçince değiştir
+            optionsBuilder.UseSqlServer("server=");
+        }
+    }
+}
--- EntitiyLayer/Concrete/Blog.cs
+++ EntitiyLayer/Concrete/Blog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,13 @@ namespace EntitiyLayer.Concrete
 {
     public class Blog
     {
+        [Key]
         public int BlogID { get; set; }
         public string BlogTitle { get; set; }
         public string BlogContent { get; set; }
-        public string BlogCreateDate { get; set; }
         public string BlogThumbnailImage { get; set; }
         public string BlogImage { get; set; }
+        public DateTime BlogCreateDate { get; set; }
         public bool BlogStatus { get; set; }
     }
 }
