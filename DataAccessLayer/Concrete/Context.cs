646036d42cd381a52b9f8123940b95000c7b0a77
TaylanCann <taylancanh@gmail.com>
2021-10-20 10:28:24 +0300

    referances

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntitiyLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,12 @@ namespace DataAccessLayer.Concrete
             //Todo başka bilgisayara geçince değiştir
             optionsBuilder.UseSqlServer("server=TAYLAN\\SQLEXPRESS;database=CoreBlogDb; integrated security=true;");
         }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
     }
 }
